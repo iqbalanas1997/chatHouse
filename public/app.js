44f09eeb77ee27ee9be783f1ce2e041f155700aa
iqbalanas1997 <57329366+iqbalanas1997@users.noreply.github.com>
2020-09-21 14:57:39 +0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 //Sir i'm working on it .. although it is incomplete for now but i've done functionality to fetch and store data in database but now i'm trying to make big
-//application lie whatsapp so it will take some time .. hope you will consider this app as in progress not incomplete assignment
+//application like whatsapp so it will take some time .. hope you will consider this app as in progress not incomplete assignment
 // And A very very thankfull to you all teachers who made me elibible to make this beautiful web application
 
 
